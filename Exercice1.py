787d19e54ae6d81403b24e29d03ce87b8aa51d5b
SofKh <sofiane.chkh@gmail.com>
2022-03-14 10:49:52 -0400

    Create Exercice1.py

---
+++ Exercice1.py
@@ -0,0 +1,29 @@
+def fonction(n1, n2, n3, n4):
+
+# solution_un = if n1<n2<n3<n4 : 
+#                   Return n1, n2, n3, n4
+#                           Return n1, n2, n4, n3
+#                           Return n1, n3, n2, n4
+#                           Return n1, n3, n4, n2
+#                           Return n1, n4, n2, n3
+#                           Return n1, n4, n3, n2
+# solution_deux = n2<n1<n3<n4 Return n2, n1, n3, n4
+#                             Return n2, n1, n4, n3
+#                             Return n2, n3, n1, n4
+#                             Return n2, n3, n4, n1
+#                             Return n2, n4, n3, n1
+#                             Return n2, n4, n1, n3
+# solution_trois = n3<n1<n2<n4 Return n3, n1, n2, n4
+#                              Return n3, n1, n4, n2
+#                              Return n3, n2, n3, n4
+#                              Return n3, n2, n4, n3
+#                              Return n3, n4, n1, n2
+#                              Return n3, n4, n2, n1
+# solution_quatre = n4<n1<n2<n3 Return n4, n1, n2, n3
+#                               Return n4, n1, n3, n2
+#                               Return n4, n2, n3, n1
+#                               Return n4, n2, n1, n3
+#                               Return n4, n3, n2, n1
+#                               Return n4, n3, n1, n2
+
+
