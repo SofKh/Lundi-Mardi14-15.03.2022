5c691d5a8fc5f5008da74666c2b5dc0c787010db
SofKh <sofiane.chkh@gmail.com>
2022-03-15 12:23:09 -0400

    changement

--- Mardi 15.Mars.Sofiane.Exercice5.py
+++ Mardi 15.Mars.Sofiane.Exercice5.py
@@ -5,11 +5,11 @@
 def generation():
     
     année=int(input("Indiquez votre année de naissance afin de savoir votre generation: "))
-    if année <= 1946 and année <= 1965:
+    if année >= 1946 and année < 1965:
         print("Vous etes de la generation Baby-boomers")
-    elif année <= 1966 and année <= 1979:
+    elif année >= 1965 and année < 1980:
         print("Vous etes de la generation X")
-    elif année <= 1980 and année <2000:
+    elif année >= 1980 and année < 2000:
         print("Vous etes de la generation Y")
     else:
         print("Vous etes de la generation Z")
